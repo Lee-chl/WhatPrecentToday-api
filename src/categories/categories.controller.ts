5b49859d78e9b085f60a1d8cd6deaadf967b015f
YJ <chlrhhan@gmail.com>
2026-06-19 01:29:36 +0900

    feat categories deving

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,15 +1,34 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth/jwt-auth.guard';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { CurrentUser } from '../common/current-user.decoraor';
 
 @Controller('categories')
+@UseGuards(JwtAuthGuard)
+@ApiBearerAuth()
+@ApiTags('categories')
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
 
   @Post()
-  create(@Body() createCategoryDto: CreateCategoryDto) {
-    return this.categoriesService.create(createCategoryDto);
+  @ApiOperation({ summary: '카테고리 추가' })
+  create(
+    @Body() createCategoryDto: CreateCategoryDto,
+    @CurrentUser('role') userRole: string,
+  ) {
+    return this.categoriesService.create(createCategoryDto, userRole);
   }
 
   @Get()
@@ -23,7 +42,10 @@ export class CategoriesController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateCategoryDto: UpdateCategoryDto,
+  ) {
     return this.categoriesService.update(+id, updateCategoryDto);
   }
 
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,10 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 
 @Injectable()
 export class CategoriesService {
-  create(createCategoryDto: CreateCategoryDto) {
+  create(createCategoryDto: CreateCategoryDto, userRole: string) {
     return 'This action adds a new category';
   }
 
@@ -22,5 +22,12 @@ export class CategoriesService {
 
   remove(id: number) {
     return `This action removes a #${id} category`;
+  }
+
+  // 권한 확인
+  private checkPermission(userRole: string) {
+    if (userRole !== 'ADMIN') {
+      throw new ForbiddenException('권한이 없습니다.');
+    }
   }
 }
