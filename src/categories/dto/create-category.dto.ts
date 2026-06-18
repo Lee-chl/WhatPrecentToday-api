051d9d9da0411d70683181a9a94c48bf22344479
YJ <chlrhhan@gmail.com>
2026-06-19 01:13:41 +0900

    feat food , category dto add 음식, 카테고리 DTO 완료

--- src/categories/dto/create-category.dto.ts
+++ src/categories/dto/create-category.dto.ts
@@ -1,1 +1,19 @@
-export class CreateCategoryDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsInt, IsString, Min } from 'class-validator';
+import { Optional } from '@nestjs/common';
+
+export class CreateCategoryDto {
+  @ApiProperty({ example: '한식' })
+  @IsString()
+  name: string;
+
+  @ApiProperty({ example: '한국의 음식' })
+  @IsString()
+  @Optional()
+  description: string = '';
+
+  @ApiProperty({ example: 1 })
+  @IsInt()
+  @Min(1)
+  foodId: number;
+}
--- src/foods/dto/create-food.dto.ts
+++ src/foods/dto/create-food.dto.ts
@@ -1,1 +1,68 @@
-export class CreateFoodDto {}
+import {
+  IsArray,
+  IsInt,
+  IsOptional,
+  IsString,
+  Min,
+  MinLength,
+} from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateFoodDto {
+  @IsString()
+  @ApiProperty({ example: '아메리카노' })
+  @MinLength(1)
+  name: string;
+
+  @ApiProperty({ example: '메가커피' })
+  @MinLength(1)
+  @IsString()
+  brandName: string;
+
+  @ApiProperty({ example: 2 })
+  @IsInt()
+  @Min(0)
+  calorie: number;
+
+  @ApiProperty({ example: 0 })
+  @IsInt()
+  @Min(0)
+  carbohydrate: number;
+
+  @ApiProperty({ example: 0.14 })
+  @IsInt()
+  @Min(0)
+  protein: number;
+
+  @ApiProperty({ example: 0 })
+  @IsInt()
+  @Min(0)
+  fat: number;
+
+  @ApiProperty({ example: 0 })
+  @IsInt()
+  @Min(0)
+  sodium: number;
+
+  @ApiProperty({ example: 10 })
+  @IsInt()
+  @Min(0)
+  @IsOptional()
+  NUTRI_AMOUNT_SERVING: number = 0;
+
+  @ApiProperty({ example: 100 })
+  @IsInt()
+  @Min(0)
+  @IsOptional()
+  SERVING_SIZE: number = 0;
+
+  @ApiProperty({ example: 710 })
+  @IsInt()
+  @Min(0)
+  total_capacity: number;
+
+  @ApiProperty({ example: 1 })
+  @IsArray()
+  @IsInt({ each: true })
+  categoryIds: number[];
+}
