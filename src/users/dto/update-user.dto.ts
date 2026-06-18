4bfbc9eef4c51c234e32731b2179e3e6d0f1795b
YJ <chlrhhan@gmail.com>
2026-06-18 22:57:22 +0900

    feat user update del 유저 업데이트 삭제 기능 구현

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,9 +1,4 @@
-import {
-  IsEmail,
-  IsOptional,
-  IsString,
-  MinLength,
-} from 'class-validator';
+import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateUserDto {
