dbabb80433838183df7d9e85dd4433adda9b4b77
yeji <chlrhhan@gmail.com>
2026-06-19 18:23:58 +0900

    feat intake-logs

--- src/intake_logs/dto/create-intake_log.dto.ts
+++ src/intake_logs/dto/create-intake_log.dto.ts
@@ -1,1 +1,44 @@
-export class CreateIntakeLogDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsInt, IsNumber, Min } from 'class-validator';
+
+export class CreateIntakeLogDto {
+  @IsInt()
+  @Min(0)
+  @ApiProperty({ example: 1 })
+  userId: number;
+
+  @IsInt()
+  @Min(0)
+  @ApiProperty({ example: 1 })
+  foodId: number;
+
+  @ApiProperty({ example: 100, description: '섭취량(g)' })
+  @Min(0)
+  @IsNumber()
+  intake: number;
+
+  @ApiProperty({ example: 10, description: '먹은 칼로리(g)' })
+  @Min(0)
+  @IsNumber()
+  calories: number;
+
+  @ApiProperty({ example: 10, description: '먹은 탄수화물(g)' })
+  @IsNumber()
+  @Min(0)
+  carbohydrate: number;
+
+  @ApiProperty({ example: 10, description: '먹은 단백질(g)' })
+  @IsNumber()
+  @Min(0)
+  protein: number;
+
+  @ApiProperty({ example: 10, description: '먹은 지방(g)' })
+  @IsNumber()
+  @Min(0)
+  fat: number;
+
+  @ApiProperty({ example: 10, description: '먹은 나트륨(g)' })
+  @IsNumber()
+  @Min(0)
+  sodium: number;
+}
--- src/intake_logs/intake_logs.controller.ts
+++ src/intake_logs/intake_logs.controller.ts
@@ -1,14 +1,32 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { IntakeLogsService } from './intake_logs.service';
 import { CreateIntakeLogDto } from './dto/create-intake_log.dto';
 import { UpdateIntakeLogDto } from './dto/update-intake_log.dto';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth/jwt-auth.guard';
+import { type AuthUser, CurrentUser } from 'src/common/current-user.decoraor';
 
 @Controller('intake-logs')
+@ApiTags('intake-logs')
+@UseGuards(JwtAuthGuard)
+@ApiBearerAuth()
 export class IntakeLogsController {
   constructor(private readonly intakeLogsService: IntakeLogsService) {}
 
   @Post()
-  create(@Body() createIntakeLogDto: CreateIntakeLogDto) {
+  create(
+    @Body() createIntakeLogDto: CreateIntakeLogDto,
+    @CurrentUser() user: AuthUser,
+  ) {
     return this.intakeLogsService.create(createIntakeLogDto);
   }
 
@@ -23,7 +41,10 @@ export class IntakeLogsController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateIntakeLogDto: UpdateIntakeLogDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateIntakeLogDto: UpdateIntakeLogDto,
+  ) {
     return this.intakeLogsService.update(+id, updateIntakeLogDto);
   }
 
