695c394fb06bc4a8c7133555dbda5b50f88c579f
YJ <chlrhhan@gmail.com>
2026-06-21 00:31:19 +0900

    fix constructor del

--- src/daily_nutrition_logs/dto/create-daily_nutrition_log.dto.ts
+++ src/daily_nutrition_logs/dto/create-daily_nutrition_log.dto.ts
@@ -1,15 +1,6 @@
 import { IsDate, IsInt, IsNumber, IsOptional, Min } from 'class-validator';
 
 export class CreateDailyNutritionLogDto {
-  constructor(data: CreateDailyNutritionLogDto) {
-    this.userId = data.userId;
-    this.calorie = data.calorie;
-    this.carbohydrate = data.carbohydrate;
-    this.protein = data.protein;
-    this.fat = data.fat;
-    this.sodium = data.sodium;
-    this.log_date = data.log_date;
-  }
   @IsInt()
   @Min(1)
   userId: number;
