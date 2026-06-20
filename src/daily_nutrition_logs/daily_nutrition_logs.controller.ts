30f1a424316a15502af65600ac140bf7f330e67d
YJ <chlrhhan@gmail.com>
2026-06-20 21:24:45 +0900

    feat dailynutrition find, findAll

--- src/daily_nutrition_logs/daily_nutrition_logs.controller.ts
+++ src/daily_nutrition_logs/daily_nutrition_logs.controller.ts
@@ -1,21 +1,38 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { DailyNutritionLogsService } from './daily_nutrition_logs.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth/jwt-auth.guard';
+import { QueryDto } from '../common/query.dto';
+import { type AuthUser, CurrentUser } from '../common/current-user.decoraor';
 
 @Controller('daily-nutrition-logs')
 @ApiTags('Daily Nutrition logs')
+@UseGuards(JwtAuthGuard)
+@ApiBearerAuth()
 export class DailyNutritionLogsController {
   constructor(
     private readonly dailyNutritionLogsService: DailyNutritionLogsService,
   ) {}
 
   @Get()
-  findAll() {
-    return this.dailyNutritionLogsService.findAll();
+  @ApiOperation({ summary: '데일리 영양 로그 조회' })
+  findAll(@Query() query: QueryDto, @CurrentUser('role') userRole: string) {
+    return this.dailyNutritionLogsService.findAll(query, userRole);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.dailyNutritionLogsService.findOne(+id);
+  @ApiOperation({ summary: '데일리 영양 로그 본인만 조회' })
+  findOne(
+    @Param('id', ParseIntPipe) id: number,
+    @CurrentUser() user: AuthUser,
+  ) {
+    return this.dailyNutritionLogsService.findOne(id, user);
   }
 }
--- src/daily_nutrition_logs/daily_nutrition_logs.service.ts
+++ src/daily_nutrition_logs/daily_nutrition_logs.service.ts
@@ -1,21 +1,44 @@
-import { Injectable } from '@nestjs/common';
-import { UpdateDailyNutritionLogDto } from './dto/update-daily_nutrition_log.dto';
-
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { QueryDto } from '../common/query.dto';
+import { type AuthUser } from '../common/current-user.decoraor';
+import { PrismaService } from '../prisma/prisma.service';
+import {
+  checkPermission,
+  checkPermissionRole,
+} from '../common/checkPermission';
 @Injectable()
 export class DailyNutritionLogsService {
-  findAll() {
-    return `This action returns all dailyNutritionLogs`;
+  constructor(private readonly prisma: PrismaService) {}
+  async findAll(query: QueryDto, userRole: string) {
+    checkPermissionRole(userRole);
+    const { page, limit } = query;
+    const [dailyNutritionLogs, total] = await Promise.all([
+      this.prisma.daily_nutrition_logs.findMany({
+        skip: (page - 1) * limit,
+        take: limit,
+        orderBy: { id: 'asc' },
+      }),
+      this.prisma.daily_nutrition_logs.count(),
+    ]);
+    return {
+      dailyNutritionLogs,
+      total,
+      page,
+      limit,
+      totalPage: Math.ceil(total / limit),
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} dailyNutritionLog`;
-  }
+  async findOne(id: number, user: AuthUser) {
+    const exists = await this.prisma.daily_nutrition_logs.findUnique({
+      where: { id },
+    });
 
-  update(id: number, updateDailyNutritionLogDto: UpdateDailyNutritionLogDto) {
-    return `This action updates a #${id} dailyNutritionLog`;
-  }
+    if (!exists) {
+      throw new NotFoundException(`해당 ${id}의 사용자 영양 기록이 없어요`);
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} dailyNutritionLog`;
+    checkPermission(user.role, user.id, exists.userId);
+    return exists;
   }
 }
